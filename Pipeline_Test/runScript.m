8deb6f6953be3f7eaac165dbabef18fb6b92b3cb
Shakeel <137197279+Sakeel-Ahamed@users.noreply.github.com>
2026-07-12 18:33:53 +0100

    Enhance runScript to accept and process extraArg
    Added functionality to handle an extra argument and conditionally log version or project information based on input.

--- Pipeline_Test/runScript.m
+++ Pipeline_Test/runScript.m
@@ -1,16 +1,23 @@
-function runScript(inputText)
+function runScript(inputText, extraArg)
 
-fprintf("Pipeline passed: %s\n", inputText);
+    fprintf("Pipeline passed: %s\n", inputText);
 
-if ~exist('output','dir')
-    mkdir('output');
+    if ~exist('output','dir')
+        mkdir('output');
+    end
+
+    fid = fopen(fullfile('output','result.txt'),'w');
+
+    fprintf(fid,"Input from pipeline:\n");
+    fprintf(fid,"%s\n",inputText);
+
+    if strcmp(inputText, 'buildAll')
+        fprintf(fid,"Version: %s\n", extraArg);
+    else
+        fprintf(fid,"Project: %s\n", extraArg);
+    end
+
+    fclose(fid);
+
+    disp("Artifact created.");
 end
-
-fid = fopen(fullfile('output','result.txt'),'w');
-
-fprintf(fid,"Input from pipeline:\n");
-fprintf(fid,"%s\n",inputText);
-
-fclose(fid);
-
-disp("Artifact created.");
